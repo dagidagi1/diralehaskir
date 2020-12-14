39b473389b4092f71b5164d88705c33c75ae7c14
Dagi <Dagi@DESKTOP-1R60F1N>
2020-12-15 00:47:10 +0200

    .

--- diralehaskir/diralehaskir.cpp
+++ diralehaskir/diralehaskir.cpp
@@ -557,7 +557,7 @@ void RealloctravelersArr(traveler* arr, int& size)
 	traveler* tmp = new traveler[size];
 	if (!tmp) cout << "Memory allocate fail!" << endl;
 	for (int i = 0; i < size - 1; i++) tmp[i] = arr[i];
-	delete[] arr;
+	if(arr) delete[] arr;
 	arr = tmp;
 }
 void ReallocLandlordsArr(landlord* arr, int size)
@@ -565,7 +565,7 @@ void ReallocLandlordsArr(landlord* arr, int size)
 	landlord* tmp = new landlord[size + 1];
 	for (int i = 0; i < size; i++) tmp[i] = arr[i];
 	size++;
-	delete[] arr;
+	if (arr) delete[] arr;
 	arr = tmp;
 }
 
